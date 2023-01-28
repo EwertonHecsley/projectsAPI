1140baa2fcaffd188f5a310cdf2e863625067a1b
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-01-28 17:20:35 -0300

    Adicionando logica

--- Busca-CEP/src/main.js
+++ Busca-CEP/src/main.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const botao = document.querySelector('.btn-cep');
+const campoCep = document.querySelector('#inp-cep');
+const areaDeTexto = document.querySelector('#textArea');
+
+
+
+
+
+async function clicou(){
+    let informacao = campoCep.value;
+
+    try {
+        const result = await fetch(`https://viacep.com.br/ws/${informacao}/json/`);
+        const data = await result.json();
+
+        areaDeTexto.innerHTML = JSON.stringify(data);
+        return data;
+    } catch (error) {
+        return error.message
+    }
+    
+};
+
+botao.addEventListener('click',clicou)
